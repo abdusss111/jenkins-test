71f7828281cb36a770eca3e31705d689fdfeda2b
Abdussalam Abdurakhimov <115406100+abdusss111@users.noreply.github.com>
2025-02-19 19:55:08 +0500

    Update print_names.py

--- print_names.py
+++ print_names.py
@@ -1,3 +1,12 @@
 import sys
-names = sys.argv
-print(*names)
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python names_cli.py name1 name2 ...")
+        return
+
+    names = sys.argv[1:]  # Get all command-line arguments except the script name
+    print("Names entered:", ", ".join(names))
+
+if __name__ == "__main__":
+    main()
